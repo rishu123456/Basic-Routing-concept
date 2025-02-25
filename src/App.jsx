0cec6009593376d14a580b5f2df4326cef93d58a
rishu123456 <rishu25112001@gamil.com>
2025-02-25 10:34:46 +0530

    basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import {BrowserRouter, Route, Routes} from "react-router-dom"
-import { Dashboard } from './components/Dashboard'
-import { Landing } from './components/Landing'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+import { Dashboard } from "./components/Dashboard";
+import { Landing } from "./components/Landing";
 
 function App() {
-  
-
   return (
     <div>
-    <div style={{backgroundColor:'black',color:"white"}}>this is style</div>
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Dashboard></Dashboard>} />
-      <Route path='/landing' element={<Landing></Landing>} />
-    </Routes>
-    
-    </BrowserRouter>
+      <div style={{ backgroundColor: "black", color: "white" }}>
+        This is style
+      </div>
+      <BrowserRouter>
+        <Navbar /> 
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/landing" element={<Landing />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
+
+function Navbar() {
+  const navigate = useNavigate();
+  return (
+    <>
+      <button onClick={() => navigate("/")}>Dashboard</button>
+      <button onClick={() => navigate("/landing")}>Landing</button>
+    </>
+  );
+}
